ca1b32602b654be7b016ce27a620d1498df74220
gaveshvimanshana101-sket <gaveshvimanshana101@gmail.com>
2026-03-09 08:40:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RBsTHArL#NHCQT5yzQw6gFeWSJvUKncwqryqDfptWcLZ9DajMFXA",
+SESSION_ID: process.env.SESSION_ID || "5I8iXSaK#4ZKlIPS1j_GyFt9Hn5oZ62tuIokYnz5lYxrrrr-buvA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Hnsk23/hnsk/blob/main/images/HANSA%20md.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIMA-MD Is Alive Now😍*",
 BOT_OWNER: '94789706579',  // Replace with the owner's phone number
